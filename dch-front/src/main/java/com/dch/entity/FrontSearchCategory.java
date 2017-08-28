52872690d85e1f1d7b6884a8914c93d0018f1be9
dasinWalk <sunkangqing@163.com>
2017-08-28 14:13:25 +0800

    [skq] 1,前端全文检索接口entity类提交

---
+++ dch-front/src/main/java/com/dch/entity/FrontSearchCategory.java
@@ -0,0 +1,158 @@
+package com.dch.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Created by Administrator on 2017/8/28.
+ */
+@Entity
+@Table(name = "front_search_category", schema = "dch", catalog = "")
+public class FrontSearchCategory {
+    private String id;
+    private String parentId;
+    private String categoryName;
+    private String dataApiPath;
+    private String viewPath;
+    private Timestamp createDate;
+    private Timestamp modifyDate;
+    private String createBy;
+    private String modifyBy;
+    private String status;
+
+    @Id
+    @Column(name = "id", nullable = false, length = 64)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "parent_id", nullable = true, length = 64)
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    @Basic
+    @Column(name = "category_name", nullable = true, length = 200)
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Basic
+    @Column(name = "data_api_path", nullable = true, length = 500)
+    public String getDataApiPath() {
+        return dataApiPath;
+    }
+
+    public void setDataApiPath(String dataApiPath) {
+        this.dataApiPath = dataApiPath;
+    }
+
+    @Basic
+    @Column(name = "view_path", nullable = true, length = 200)
+    public String getViewPath() {
+        return viewPath;
+    }
+
+    public void setViewPath(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    @Basic
+    @Column(name = "create_date", nullable = true)
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    @Basic
+    @Column(name = "modify_date", nullable = true)
+    public Timestamp getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Timestamp modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Basic
+    @Column(name = "create_by", nullable = true, length = 64)
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    @Basic
+    @Column(name = "modify_by", nullable = true, length = 64)
+    public String getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(String modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = true, length = 2)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FrontSearchCategory that = (FrontSearchCategory) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (parentId != null ? !parentId.equals(that.parentId) : that.parentId != null) return false;
+        if (categoryName != null ? !categoryName.equals(that.categoryName) : that.categoryName != null) return false;
+        if (dataApiPath != null ? !dataApiPath.equals(that.dataApiPath) : that.dataApiPath != null) return false;
+        if (viewPath != null ? !viewPath.equals(that.viewPath) : that.viewPath != null) return false;
+        if (createDate != null ? !createDate.equals(that.createDate) : that.createDate != null) return false;
+        if (modifyDate != null ? !modifyDate.equals(that.modifyDate) : that.modifyDate != null) return false;
+        if (createBy != null ? !createBy.equals(that.createBy) : that.createBy != null) return false;
+        if (modifyBy != null ? !modifyBy.equals(that.modifyBy) : that.modifyBy != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (parentId != null ? parentId.hashCode() : 0);
+        result = 31 * result + (categoryName != null ? categoryName.hashCode() : 0);
+        result = 31 * result + (dataApiPath != null ? dataApiPath.hashCode() : 0);
+        result = 31 * result + (viewPath != null ? viewPath.hashCode() : 0);
+        result = 31 * result + (createDate != null ? createDate.hashCode() : 0);
+        result = 31 * result + (modifyDate != null ? modifyDate.hashCode() : 0);
+        result = 31 * result + (createBy != null ? createBy.hashCode() : 0);
+        result = 31 * result + (modifyBy != null ? modifyBy.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
+}
