3d9bc69e2acb5142a747a2e5f8736a5e0a482ab3
Administrator <zhaotieqiang@gmail.com>
2017-09-19 09:32:47 +0800

    [ztq] 1，添加病例管理模块 2，添加病例管理实体类

---
+++ dch-mr/src/main/java/com/dch/entity/MrFile.java
@@ -0,0 +1,84 @@
+package com.dch.entity;
+
+import com.dch.entity.base.BaseEntity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Created by Administrator on 2017/9/19.
+ */
+@Entity
+@Table(name = "mr_file", schema = "dch", catalog = "")
+public class MrFile extends BaseEntity {
+
+    private String fileTitle;
+    private String subjectCode;
+    private String status;
+    private String fileContent;
+    private String attachmentFileId;
+    private String keyWords;
+
+
+
+    @Basic
+    @Column(name = "file_title", nullable = true, length = 200)
+    public String getFileTitle() {
+        return fileTitle;
+    }
+
+    public void setFileTitle(String fileTitle) {
+        this.fileTitle = fileTitle;
+    }
+
+    @Basic
+    @Column(name = "subject_code", nullable = true, length = 64)
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = true, length = 2)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "file_content", nullable = true, length = -1)
+    public String getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(String fileContent) {
+        this.fileContent = fileContent;
+    }
+
+    @Basic
+    @Column(name = "attachment_file_id", nullable = true, length = 64)
+    public String getAttachmentFileId() {
+        return attachmentFileId;
+    }
+
+    public void setAttachmentFileId(String attachmentFileId) {
+        this.attachmentFileId = attachmentFileId;
+    }
+
+    @Basic
+    @Column(name = "key_words", nullable = true, length = 500)
+    public String getKeyWords() {
+        return keyWords;
+    }
+
+    public void setKeyWords(String keyWords) {
+        this.keyWords = keyWords;
+    }
+
+}
--- dch-mr/src/main/java/com/dch/entity/MrSubject.java
+++ dch-mr/src/main/java/com/dch/entity/MrSubject.java
@@ -6,13 +6,15 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 /**
- * Created by Administrator on 2017/9/15.
+ * Created by Administrator on 2017/9/19.
  */
 @Entity
 @Table(name = "mr_subject", schema = "dch", catalog = "")
 public class MrSubject extends BaseEntity {
     private String subjectName;
     private String parentSubjectId;
+
+
 
     @Basic
     @Column(name = "subject_name", nullable = true, length = 200)
@@ -33,5 +35,7 @@ public class MrSubject extends BaseEntity {
     public void setParentSubjectId(String parentSubjectId) {
         this.parentSubjectId = parentSubjectId;
     }
+
+
 
 }
